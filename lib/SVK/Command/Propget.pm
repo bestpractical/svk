37bce8af4f7e9d91049fb54eba19be48f85b487d
Audrey Tang <audreyt@audreyt.org>
2004-09-13 23:00:11 +0000

    * add the missing Propget
    git-svn-id: file:///home/chmrr/work/bps/complete-history/svk/orig-svn/branches/win32@402 4ccb8a5b-8ae0-0310-9b02-86531366991d

---
+++ lib/SVK/Command/Propget.pm
@@ -0,0 +1,69 @@
+package SVK::Command::Propget;
+use strict;
+our $VERSION = $SVK::VERSION;
+
+use base qw( SVK::Command );
+use SVK::XD;
+use SVK::Util qw(get_buffer_from_editor);
+
+sub options {
+    ('strict'	=> 'strict',
+    );
+}
+
+sub parse_arg {
+    my ($self, @arg) = @_;
+    return if @arg < 1;
+    push @arg, '.' if @arg == 1;
+    return ($arg[0], map {$self->arg_co_maybe ($_)} @arg[1..$#arg]);
+}
+
+sub lock { $_[0]->lock_none }
+
+sub run {
+    my ($self, $pname, @targets) = @_;
+
+    foreach my $target (@targets) {
+        my $proplist = $self->{xd}->do_proplist ($target);
+        exists $proplist->{$pname} or next;
+
+        print $target->path, ' - ' if @targets > 1 and !$self->{strict};
+        print $proplist->{$pname};
+        print "\n" if !$self->{strict};
+    }
+
+    return;
+}
+
+1;
+
+__DATA__
+
+=head1 NAME
+
+SVK::Command::Propget - Display a property on path
+
+=head1 SYNOPSIS
+
+ propget PROPNAME [PATH|DEPOTPATH...]
+
+=head1 OPTIONS
+
+ --strict:                  Do not print an extra newline at the end of the
+                            property values; when there are multiple paths
+                            involved, do not prefix path names before values
+
+=head1 AUTHORS
+
+Chia-liang Kao E<lt>clkao@clkao.orgE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2003-2004 by Chia-liang Kao E<lt>clkao@clkao.orgE<gt>.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+See L<http://www.perl.com/perl/misc/Artistic.html>
+
+=cut
