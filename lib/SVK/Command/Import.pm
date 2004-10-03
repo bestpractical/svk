aa53970ff54e9eeecd78361ca65a73bce847a034
Audrey Tang <audreyt@audreyt.org>
2004-10-03 22:40:49 +0000

    * "svk import //mirror/path path" should not silently corrupt things!
    git-svn-id: file:///home/chmrr/work/bps/complete-history/svk/orig-svn/trunk@595 4ccb8a5b-8ae0-0310-9b02-86531366991d

--- lib/SVK/Command/Import.pm
+++ lib/SVK/Command/Import.pm
@@ -59,6 +59,7 @@ sub mkpdir {
 
 sub run {
     my ($self, $target, $copath) = @_;
+    return unless $self->check_mirrored_path ($target);
 
     my $fs = $target->{repos}->fs;
     my $yrev = $fs->youngest_rev;
