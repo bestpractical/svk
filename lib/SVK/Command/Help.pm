054c0f7eb59517089521cd743a7675aad794ad91
Audrey Tang <audreyt@audreyt.org>
2004-10-17 04:59:07 +0000

    * The SVK Help system, as SVK/Help/[lang/]/*.pod. * Regenerate manifest.
    git-svn-id: file:///home/chmrr/work/bps/complete-history/svk/orig-svn/trunk@662 4ccb8a5b-8ae0-0310-9b02-86531366991d

--- lib/SVK/Command/Help.pm
+++ lib/SVK/Command/Help.pm
@@ -4,23 +4,80 @@ our $VERSION = $SVK::VERSION;
 
 use base qw( SVK::Command );
 use SVK::I18N;
+use Pod::Simple::Text ();
 
-sub parse_arg { shift; @_ ? @_ : ''; }
+sub parse_arg { shift; @_ ? @_ : 'index'; }
 
 sub run {
     my $self = shift;
-    unless ($_[0]) {
-	my @cmd;
-	my $dir = $INC{'SVK/Command.pm'};
-	$dir =~ s/\.pm$//;
-	print loc("Available commands:\n");
-	File::Find::find (sub {
-			      push @cmd, $File::Find::name if m/\.pm$/;
-			  }, $dir);
-	$self->brief_usage ($_) for sort @cmd;
-    } else {
-        $self->get_cmd ($_)->usage(1) foreach @_;
+    foreach my $topic (@_) {
+        if ($topic eq 'commands') {
+            my @cmd;
+            my $dir = $INC{'SVK/Command.pm'};
+            $dir =~ s/\.pm$//;
+            print loc("Available commands:\n");
+            File::Find::find (
+                sub { push @cmd, $File::Find::name if m/\.pm$/ }, $dir,
+            );
+            $self->brief_usage ($_) for sort @cmd;
+        }
+        elsif (my $cmd = eval { $self->get_cmd ($topic) }) {
+            $cmd->usage(1);
+        }
+        elsif (my $file = $self->_find_topic($topic)) {
+            open my $fh, '<:utf8', $file or die $!;
+            my $parser = Pod::Simple::Text->new;
+            my $buf;
+            $parser->output_string(\$buf);
+            $parser->parse_file($fh);
+
+            $buf =~ s/^NAME\s+SVK::Help::(.+)\s+DESCRIPTION/    $1:/;
+
+            require Encode;
+            print Encode::encode($self->_find_encoding, $buf);
+        }
+        else {
+            warn loc("Cannot find help topic '%1'.\n", $topic);
+        }
     }
+    return;
+}
+
+sub _find_topic {
+    my ($self, $topic) = @_;
+
+    return (
+        $self->_find_in_inc(loc("SVK::Help")."::\u\L$topic")
+        || $self->_find_in_inc(loc("SVK::Help")."::\U$topic")
+        || $self->_find_in_inc("SVK::Help::\u\L$topic")
+        || $self->_find_in_inc("SVK::Help::\U$topic")
+    );
+}
+
+sub _find_encoding {
+    local $@;
+    return eval {
+        local $Locale::Maketext::Lexicon::Opts{encoding} = 'locale';
+        Locale::Maketext::Lexicon::encoding();
+    } || eval {
+        require 'open.pm';
+        return open::_get_locale_encoding();
+    } || 'utf8';
+}
+
+sub _find_in_inc {
+    my ($self, $file) = @_;
+    
+    $file =~ s{::}{/}g;
+
+    # absolute file names
+    return $file if -f $file;
+
+    foreach my $dir (@INC) {
+        return "$dir/$file.pod" if -f "$dir/$file.pod";
+        return "$dir/$file.pm"  if -f "$dir/$file.pm";
+    }
+    return;
 }
 
 1;
