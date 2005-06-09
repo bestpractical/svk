47cc30c256d8ac3f1371ecc350e0b25dd2e16d08
Chia-liang Kao <clkao@bestpractical.com>
2005-06-09 17:35:10 +0000

    Don't bother return since we call die already.
    git-svn-id: file:///home/chmrr/work/bps/complete-history/svk/orig-svn/trunk@1195 4ccb8a5b-8ae0-0310-9b02-86531366991d

--- lib/SVK/Command/List.pm
+++ lib/SVK/Command/List.pm
@@ -43,7 +43,6 @@ sub _do_list {
     unless ((my $kind = $root->check_path ($target->{path})) == $SVN::Node::dir) {
        die loc("Path %1 is not a versioned directory\n", $target->{path})
            unless $kind == $SVN::Node::file;
-       return;
     }
 
     # XXX: SVK::Target should take care of this.
