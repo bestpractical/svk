55d8b99ed4f705262d3d3f1911af93b0c712c94b
Michael Brouwer <michael@tlaloc.net>
2005-11-17 17:29:51 +0000

    Fixed crash in t/07smerge.t when using svn-1.3.0-rc1 introduced by 1609.
    Allocate temporary objects in $spool rather than mixing $spool and
    $new_pool.  Since $spool is cleared each iteration and $new_pool
    lasts for 2 iterations.
    
    git-svn-id: file:///home/chmrr/work/bps/complete-history/svk/orig-svn/trunk@1621 4ccb8a5b-8ae0-0310-9b02-86531366991d

--- lib/SVK/Util.pm
+++ lib/SVK/Util.pm
@@ -854,30 +854,30 @@ sub traverse_history {
             # prev if we ask svn to traverse copies.
             # Let's find out if the copy was actually a rename instead
             # of a copy.
-            my $root = $args{root}->fs->revision_root($revision);
+            my $root = $args{root}->fs->revision_root($revision, $spool);
             my $frompath;
             my $fromrev = -1;
             # We know that $path was a real copy and it that it has a
             # prev, so find the node from which it was copied.
-            while ($fromrev < 0) {
-                ($fromrev, $frompath) = $root->copied_from($path);
-                last unless $path =~ s{/[^/]*$}{};
-            }
+            do {
+                ($fromrev, $frompath) = $root->copied_from($path, $spool);
+            } until ($fromrev >= 0 || !($path =~ s{/[^/]*$}{}));
             die "Assertion failed: $path in $revision isn't a copy."
                 if $fromrev < 0;
             # Ok, $path in $root was a copy of ($frompath,$fromrev).
             # If $frompath was deleted in $root then the copy was really
             # a rename.
-            my $entry = $root->paths_changed($new_pool)->{$frompath};
+            my $entry = $root->paths_changed($spool)->{$frompath};
             last unless $entry &&
                 $entry->change_kind == $SVN::Fs::PathChange::delete;
 
             # XXX Do we need to worry about a parent of $frompath having
-            # been deleted instead?  If so the line below might work as an
-            # alternative, to the previous 3 lines.  However this also
+            # been deleted instead?  If so the 2 lines below might work as
+            # an alternative, to the previous 3 lines.  However this also
             # treats a delete followed by a copy of an older revision in
             # two separate commits as a rename, which technically it's not.
-            #last unless $root->check_path($frompath) == $SVN::Node::none;
+            #last unless $root->check_path($frompath, $spool) ==
+            #    $SVN::Node::none;
         }
         ($path, $revision) = $hist->location ($new_pool);
         $rv = $args{callback}->($path, $revision);
