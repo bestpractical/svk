22f3c890f5a369009359eb161e562c3e0248c66a
Chia-liang Kao <clkao@bestpractical.com>
2006-10-17 05:20:15 +0000

    locking related stuff in svk::mirror.
    git-svn-id: file:///home/chmrr/work/bps/complete-history/svk/orig-svn/branches/svkmirror-refactor@1984 4ccb8a5b-8ae0-0310-9b02-86531366991d

--- lib/SVK/Mirror.pm
+++ lib/SVK/Mirror.pm
@@ -2,9 +2,11 @@ package SVK::Mirror;
 use strict;
 use warnings;
 
+use Sys::Hostname;
+
 use base 'Class::Accessor::Fast';
 
-__PACKAGE__->mk_accessors(qw(repos path _backend));
+__PACKAGE__->mk_accessors(qw(repos path _backend _locked));
 
 =head1 NAME
 
@@ -13,7 +15,7 @@ SVK::Mirror -
 =head1 SYNOPSIS
 
     # setup a new mirror
-    my $mirror = SVK::Mirror->new( { backend => 'svnra',  url => 'http://server/',
+    my $mirror = SVK::Mirror->new( { backend => 'SVNRa',  url => 'http://server/',
                                      backend_options => {}, repos => $repos, path => '/mirror' } );
     # load existing mirror
     my $existing = SVK::Mirror->load( { path => $path, repos => $repos } );
@@ -31,10 +33,12 @@ SVK::Mirror -
 =cut
 
 sub new {
-    my ($class, $args) = @_;
+    my ( $class, $args ) = @_;
     my $self = $class->SUPER::new($args);
 
-    $self->_backend( $self->_create_backend($args->{backend}, $args->{backend_options}) );
+    $self->_backend(
+        $self->_create_backend( $args->{backend}, $args->{backend_options} )
+    );
 
     SVK::MirrorCatalog->add_mirror($self);
 
@@ -46,9 +50,18 @@ sub new {
 =cut
 
 sub load {
-    my ($class, $args) = @_;
+    my ( $class, $args ) = @_;
     my $self = $class->SUPER::new($args);
 
+    $self->_backend( $self->_load_backend );
+
+    return $self;
+}
+
+sub _load_backend {
+    my ($self) = @_;
+
+    return SVK::Mirror::Backend::SVNRa->new();
 }
 
 =back
@@ -56,6 +69,68 @@ sub load {
 =head2 METHODS
 
 =over
+
+=item detach
+
+=item relocate($newurl)
+
+=item with_lock($code)
+
+=cut
+
+sub with_lock {
+    my ( $self, $lock, $code ) = @_;
+
+    $self->lock;
+    $code->();
+    $self->unlock;
+
+}
+
+sub _lock_token {
+    my $token = $_[0]->path;
+    $token =~ s/_/__/g;
+    $token =~ s{/}{_}g;
+    return "svm:lock:$token";
+}
+
+sub _lock {
+    my ($self)  = @_;
+    my $fs      = $self->repos->fs;
+    my $token   = $self->_lock_token;
+    my $content = hostname . ':' . $$;
+    my $where = join( ' ', ( caller(0) )[ 0 .. 2 ] );
+
+    # This is not good enough but race condition should result in failed sync
+    # without corrupting repository.
+LOCKED:
+    {
+        while (1) {
+            my $who = $fs->revision_prop( 0, $token ) or last LOCKED;
+            print loc( "Waiting for lock on %1: %2.\n", $self->path, $who );
+            sleep 1;
+        }
+    }
+    $fs->change_rev_prop( 0, $token, $content );
+    $self->_locked(1);
+}
+
+sub _unlock {
+    my ( $self, $force ) = @_;
+    my $fs = $self->repos->fs;
+    if ($force) {
+        for ( keys %{ $fs->revision_proplist(0) } ) {
+            $fs->change_rev_prop( 0, $_, undef );
+        }
+        return;
+    }
+
+    my $token = $self->_lock_token;
+    if ( $self->_locked ) {
+        $fs->change_rev_prop( 0, $token, undef );
+        $self->_locked(0);
+    }
+}
 
 =item find_changeset($localrev)
 
@@ -65,13 +140,7 @@ sub load {
 
 =item mirror_changesets
 
-=item detach
-
 =item get_commit_editor
-
-=item relocate($newurl)
-
-=item with_lock($code)
 
 =item move($newpath)
 
@@ -86,8 +155,6 @@ sub url { $_[0]->_backend->url }
 =back
 
 =cut
-
-
 
 package SVK::Mirror::ChangeSet;
 use base 'Class::Accessor::Fast';
