b95adfe5b65d3002542e4a84416be140910c4250
Jesse Vincent <jesse@bestpractical.com>
2006-11-16 02:57:56 +0000

    (empty commit message)
    git-svn-id: file:///home/chmrr/work/bps/complete-history/svk/orig-svn/trunk@2145 4ccb8a5b-8ae0-0310-9b02-86531366991d

--- lib/SVK/Command/Copy.pm
+++ lib/SVK/Command/Copy.pm
@@ -155,7 +155,7 @@ sub run {
     my $m = $self->under_mirror($dst);
     if ( $m && !$dst->same_source(@src) ) {
         print loc("You are trying to copy across different mirrors.\n");
-        die loc( "Try create an empty directory %1, and run smerge --baseless %2 %3.\n",
+        die loc( "Create an empty directory %1, and run smerge --baseless %2 %3.\n",
             $dst->report, $src[0]->report, $dst->report )
           if $#src == 0 && $dst->isa('SVK::Path');
         return 1;
