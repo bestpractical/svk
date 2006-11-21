e3734886720621246adb24d82ebc0a90eed1736c
Chia-liang Kao <clkao@bestpractical.com>
2006-11-21 08:29:37 +0000

    Replace copyright section of pod in individual file with bps tagged license section that can be updated easily.
    git-svn-id: file:///home/chmrr/work/bps/complete-history/svk/orig-svn/trunk@2190 4ccb8a5b-8ae0-0310-9b02-86531366991d

--- lib/SVK/MimeDetect/FileMMagic.pm
+++ lib/SVK/MimeDetect/FileMMagic.pm
@@ -1,18 +1,53 @@
-package SVK::MimeDetect::FileMMagic;
-use strict;
-use warnings;
-use base qw( File::MMagic );
-
-use SVK::Util qw( is_binary_file );
-
-=for Workaround:
-
-File::MMagic 1.27 doesn't correctly handle subclassing.  The object returned by
-new is blessed into 'File::MMagic' instead of the subclass.  The author has
-accepted a patch to correct this behavior.  Once the patched version is
-released on CPAN, new() should be removed and the fixed version required.
-
-=cut
+# BEGIN BPS TAGGED BLOCK {{{
+# COPYRIGHT:
+# 
+# This software is Copyright (c) 2003-2006 Best Practical Solutions, LLC
+#                                          <clkao@bestpractical.com>
+# 
+# (Except where explicitly superseded by other copyright notices)
+# 
+# 
+# LICENSE:
+# 
+# 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of either:
+# 
+#   a) Version 2 of the GNU General Public License.  You should have
+#      received a copy of the GNU General Public License along with this
+#      program.  If not, write to the Free Software Foundation, Inc., 51
+#      Franklin Street, Fifth Floor, Boston, MA 02110-1301 or visit
+#      their web page on the internet at
+#      http://www.gnu.org/copyleft/gpl.html.
+# 
+#   b) Version 1 of Perl's "Artistic License".  You should have received
+#      a copy of the Artistic License with this package, in the file
+#      named "ARTISTIC".  The license is also available at
+#      http://opensource.org/licenses/artistic-license.php.
+# 
+# This work is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+# 
+# CONTRIBUTION SUBMISSION POLICY:
+# 
+# (The following paragraph is not intended to limit the rights granted
+# to you to modify and distribute this software under the terms of the
+# GNU General Public License and is only of importance to you if you
+# choose to contribute your changes and enhancements to the community
+# by submitting them to Best Practical Solutions, LLC.)
+# 
+# By intentionally submitting any modifications, corrections or
+# derivatives to this work, or any other work intended for use with
+# Request Tracker, to Best Practical Solutions, LLC, you confirm that
+# you are the copyright holder for those contributions and you grant
+# Best Practical Solutions, LLC a nonexclusive, worldwide,
+# irrevocable, royalty-free, perpetual, license to use, copy, create
+# derivative works based on those contributions, and sublicense and
+# distribute those contributions and any derivatives thereof.
+# 
+# END BPS TAGGED BLOCK }}}
 sub new {
     my $pkg = shift;
     my $new_self = $pkg->SUPER::new(@_);
@@ -48,17 +83,4 @@ SVK::MimeDetect::FileMMagic
 =head1 DESCRIPTION
 
 Implement MIME type detection using the module File::MMagic.
-
-=head1 AUTHORS
-
-Michael Hendricks E<lt>michael@ndrix.orgE<gt>
-
-=head1 COPYRIGHT
-
-Copyright 2003-2006 by Chia-liang Kao E<lt>clkao@clkao.orgE<gt>.
-
-This program is free software; you can redistribute it and/or modify it
-under the same terms as Perl itself.
-
-See L<http://www.perl.com/perl/misc/Artistic.html>
 
