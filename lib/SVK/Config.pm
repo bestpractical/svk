e0548de8a35b7a384cf5a0751dec83cf76e5cc2b
Chia-liang Kao <clkao@bestpractical.com>
2006-12-21 16:09:06 +0000

    If svnconfig is already loaded, use it even SVKNOSVNCONFIG is set.
    git-svn-id: file:///home/chmrr/work/bps/complete-history/svk/orig-svn/branches/2.0-releng@2253 4ccb8a5b-8ae0-0310-9b02-86531366991d

--- lib/SVK/Config.pm
+++ lib/SVK/Config.pm
@@ -89,9 +89,9 @@ my $pool = SVN::Pool->new;
 
 sub svnconfig {
     my $class = shift;
-    return undef if $ENV{SVKNOSVNCONFIG};
-
     return $class->_svnconfig if $class->_svnconfig;
+
+    return undef if $ENV{SVKNOSVNCONFIG};
 
     SVN::Core::config_ensure(undef);
     return $class->_svnconfig( SVN::Core::config_get_config(undef, $pool) );
