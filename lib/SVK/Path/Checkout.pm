6968ecb50714fcc82d1665293660017be6e2562a
Chia-liang Kao <clkao@bestpractical.com>
2006-12-24 15:00:49 +0000

    Fix create_xd_root for win32 with mixed revision checkout.
    git-svn-id: file:///home/chmrr/work/bps/complete-history/svk/orig-svn/branches/2.0-releng@2262 4ccb8a5b-8ae0-0310-9b02-86531366991d

--- lib/SVK/Path/Checkout.pm
+++ lib/SVK/Path/Checkout.pm
@@ -106,11 +106,9 @@ sub create_xd_root {
     my (undef, $coroot) = $self->xd->{checkout}->get($copath, 1);
     Carp::cluck $copath.YAML::Syck::Dump($self->xd->{checkout}) unless $coroot;
     my @paths = $self->xd->{checkout}->find($coroot, {revision => qr'.*'});
-    my $tmp = $copath;
-    $tmp =~ s/^\Q$coroot//;
+    my $tmp = $self->_to_pclass($copath)->relative($coroot)->as_foreign('Unix');
     my $coroot_path = $self->path;
-    $coroot_path =~ s/\Q$tmp\E$// or return $self->source->root;
-    $coroot_path = '/' unless length $coroot_path;
+    $coroot_path =~ s/\Q$tmp\E$//;
 
     my $base_root = $self->source->root;
     return $base_root if $#paths <= 0;
