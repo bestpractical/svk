bestpractical/svk

commit 26b16763578815fe2aaa671fcb4c927d8c027e8c

Fix diff with copies and modified files, which tries to do cb_baseprop.
Reported by: Jubal

git-svn-id: file:///home/chmrr/work/bps/complete-history/svk/orig-svn/trunk@1541 4ccb8a5b-8ae0-0310-9b02-86531366991d

lib/SVK/Editor/Diff.pm
@@ -17,7 +17,6 @@ SVK::Editor::Diff - An editor for producing textual diffs
  $editor = SVK::Editor::Diff->new
     ( base_root   => $root,
       base_target => $target,
-      cb_baseprop    => sub { ... },
       cb_llabel      => sub { ... },
       # or llabel => 'revision <left>',
       cb_rlabel      => sub { ... },
@@ -91,6 +90,22 @@ sub retrieve_base {
     return $root->file_contents("$basepath", $pool);
 }
 
+# XXX: cleanup
+sub retrieve_base_prop {
+    my ($self, $path, $prop, $pool) = @_;
+    my ($basepath, $fromrev) = $self->_resolve_base($path);
+    $basepath = $path unless defined $basepath;
+
+    my $root = $fromrev ? $self->{base_root}->fs->revision_root($fromrev, $pool)
+	: $self->{base_root};
+
+    $basepath = "$self->{base_target}{path}/$path"
+	if $basepath !~ m{^/};
+
+    return $root->check_path($path, $pool) == $SVN::Node::none ?
+	undef : $root->node_prop($path, $prop, $pool);
+}
+
 sub apply_textdelta {
     my ($self, $path, $checksum, $pool) = @_;
     return unless $path;
@@ -102,7 +117,7 @@ sub apply_textdelta {
 	my $newtype = $info->{prop} && $info->{prop}{'svn:mime-type'};
 	my $is_text = !$newtype || mimetype_is_text ($newtype);
 	if ($is_text && !$info->{added}) {
-	    my $basetype = $self->{cb_baseprop}->($path, 'svn:mime-type', $pool);
+	    my $basetype = $self->retrieve_base_prop($path, 'svn:mime-type', $pool);
 	    $is_text = !$basetype || mimetype_is_text ($basetype);
 	}
 	unless ($is_text) {
@@ -231,7 +246,7 @@ sub output_prop_diff {
 	for (sort keys %{$self->{info}{$path}{prop}}) {
 	    $self->_print(loc("Name: %1\n", $_));
 	    my $baseprop;
-	    $baseprop = $self->{cb_baseprop}->($path, $_, $pool)
+	    $baseprop = $self->retrieve_base_prop($path, $_, $pool)
 		unless $self->{info}{$path}{added};
             my @args =
                 map \$_,

lib/SVK/Command/Diff.pm
@@ -71,13 +71,7 @@ sub run {
     my $editor = $self->{summarize} ?
 	SVK::Editor::Status->new
 	: SVK::Editor::Diff->new
-	( cb_baseprop =>
-	  sub { my ($rpath, $pname, $pool) = @_;
-		my $path = "$target->{path}/$rpath";
-		return $oldroot->check_path ($path, $pool) == $SVN::Node::none ?
-		    undef : $oldroot->node_prop ($path, $pname, $pool);
-	    },
-	  $cb_llabel ? (cb_llabel => $cb_llabel) : (llabel => "revision ".($target->{revision})),
+	( $cb_llabel ? (cb_llabel => $cb_llabel) : (llabel => "revision ".($target->{revision})),
 	  rlabel => $target2->{copath} ? 'local' : "revision ".($target2->{revision}),
 	  external => $ENV{SVKDIFF},
 	  $target->{path} ne $target2->{path} ?

lib/SVK/Patch.pm
@@ -212,10 +212,7 @@ sub view {
     my $anchor = $self->{_target}->path;
     $self->editor->drive
 	( SVK::Editor::Diff->new
-	  ( cb_baseprop => sub { my ($path, $pname, $pool) = @_;
-				 return $baseroot->node_prop ("$anchor/$path", $pname, $pool);
-			     },
-	    base_target => $self->{_target}, base_root => $baseroot,
+	  ( base_target => $self->{_target}, base_root => $baseroot,
 	    llabel => "revision $self->{target}{rev}",
 	    rlabel => "patch $self->{name} level $self->{level}",
 	    external => $ENV{SVKDIFF},

commit 617b2f70ace38c106d7c05533e4b84b1e098007b

Fix a regression introduced in r1541 which caused binary files to sometimes be displayed as text during an "svk diff".  Unfortunately, I wasn't able to create a failing .t
git-svn-id: file:///home/chmrr/work/bps/complete-history/svk/orig-svn/trunk@2291 4ccb8a5b-8ae0-0310-9b02-86531366991d

lib/SVK/Editor/Diff.pm
@@ -164,8 +164,8 @@ sub retrieve_base_prop {
     $basepath = $self->{base_target}->path_anchor."/$path"
 	if $basepath !~ m{^/};
 
-    return $root->check_path($path, $pool) == $SVN::Node::none ?
-	undef : $root->node_prop($path, $prop, $pool);
+    return $root->check_path($basepath, $pool) == $SVN::Node::none ?
+	undef : $root->node_prop($basepath, $prop, $pool);
 }
 
 sub apply_textdelta {