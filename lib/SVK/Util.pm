bestpractical/svk

commit 2ba2fa174a6d3ec61e7631a78e38ad61cdf503ee

Date: Thu, 23 Mar 2006 15:32:49 -0700 To: svk-dev@lists.openfoundry.org From: Michael Hendricks <michael@ndrix.org>
I've attached a patch which removes SVK's dependency on the TimeDate
CPAN module.  Essentially, it steals the important guts from TimeDate
and makes use of POSIX::strftime for date formatting.

As an additional bonus, the log command now runs about 25% faster when
generating long logs.

This is the second patch in a series with the theme: "Reduce SVK's
dependencies where it makes sense".  I sent the first patch to the list
some time ago and it removed the dependency on Pod::Simple.  This patch
does not depend on that one.


Log:
 r1763@askos:  michael | 2006-03-17 20:43:03 -0700
 Creating a local branch for removing the dependency on Date::Parse.
 r1764@askos:  michael | 2006-03-17 20:44:29 -0700
 Replaced one occurence of str2time with a custom str2time based on the raw Date::Parse::str2time code with optimizations for the svn:date case.
 r1765@askos:  michael | 2006-03-17 21:31:41 -0700
 Make use of the new str2time replacement subroutine
 r1766@askos:  michael | 2006-03-23 14:59:15 -0700
 Created a new my_time2str sub to replace the one from Date::Format
 r1767@askos:  michael | 2006-03-23 15:05:35 -0700
 Use the new my_time2str sub to replate time2str in SVK::Command::Log.
 r1768@askos:  michael | 2006-03-23 15:19:25 -0700
 Cleaned up the helper subs a little bit

git-svn-id: file:///home/chmrr/work/bps/complete-history/svk/orig-svn/trunk@1763 4ccb8a5b-8ae0-0310-9b02-86531366991d

Makefile.PL
@@ -26,7 +26,6 @@ requires(
         'PerlIO::via::symlink'  => '0.02',
         'IO::Digest'		=> '0',
         'SVN::Simple::Edit'     => '0.27',
-        'Date::Parse'           => '0',
         'File::Type'            => '0',
         'URI'                   => '0',
         'PerlIO::eol'           => '0.13',

lib/SVK/Util.pm
@@ -19,6 +19,8 @@ our @EXPORT_OK = qw(
     catdepot abs_path_noexist 
 
     is_symlink is_executable is_uri can_run
+
+    str2time time2str reformat_svn_date
 );
 use SVK::Version;  our $VERSION = $SVK::VERSION;
 
@@ -949,6 +951,55 @@ sub find_prev_copy {
 	for $startrev..$endrev;
     return unless $rev;
     return ($root, $copy);
+}
+
+sub reformat_svn_date {
+    my ($format, $svn_date) = @_;
+    return time2str($format, str2time($svn_date));
+}
+
+sub str2time {
+    require Time::Local;
+    my ($year, $month, $day, $hh, $mm, $ss) = split /[-T:]/, $_[0];
+    $year -= 1900;
+    $month--;
+    chop($ss);  # remove the 'Z'
+    my $zone = 0;  # UTC
+
+    my @lt = localtime(time);
+
+    my $frac = $ss - int($ss);
+    $ss = int $ss;
+
+    for ( $year, $month, $day, $hh, $mm, $ss ) {
+        return undef unless defined($_) 
+    }
+    return undef
+      unless ( $month <= 11
+        && $day >= 1
+        && $day <= 31
+        && $hh <= 23
+        && $mm <= 59
+        && $ss <= 59 );
+
+    my $result;
+
+    $result = eval {
+        local $SIG{__DIE__} = sub { };    # Ick!
+        Time::Local::timegm( $ss, $mm, $hh, $day, $month, $year );
+    };
+    return undef
+        if !defined $result
+        or $result == -1
+        && join( "", $ss, $mm, $hh, $day, $month, $year ) ne "595923311169";
+
+    return $result + $frac;
+}
+
+sub time2str {
+    my ($format, $time) = @_;
+    require POSIX;
+    return POSIX::strftime($format, localtime($time) );
 }
 
 

lib/SVK/Command/List.pm
@@ -6,9 +6,7 @@ use base qw( SVK::Command );
 use constant opt_recursive => 0;
 use SVK::XD;
 use SVK::I18N;
-use SVK::Util qw( to_native get_encoder );
-use Date::Parse qw(str2time);
-use Date::Format qw(time2str);
+use SVK::Util qw( to_native get_encoder reformat_svn_date );
 
 sub options {
     ('r|revision=s'  => 'rev',
@@ -66,7 +64,7 @@ sub _do_list {
             printf "%7ld %-8.8s %10s %12s ", $rev,
                 $fs->revision_prop ($rev, 'svn:author'),
                 ($isdir) ? "" : $root->file_length ($target->path."/$_"),
-		time2str ("%b %d %H:%M", str2time ($svn_date));
+                reformat_svn_date("%b %d %H:%M", $svn_date);
         }
 
         if ($self->{'fullpath'}) {

lib/SVK/Command/Log.pm
@@ -5,10 +5,8 @@ use SVK::Version;  our $VERSION = $SVK::VERSION;
 use base qw( SVK::Command );
 use SVK::XD;
 use SVK::I18N;
-use SVK::Util qw( traverse_history get_encoding );
+use SVK::Util qw( traverse_history get_encoding reformat_svn_date );
 use List::Util qw(max min);
-use Date::Parse qw(str2time);
-use Date::Format qw(time2str);
 
 sub options {
     ('l|limit=i'	=> 'limit',
@@ -141,7 +139,7 @@ sub _show_log {
     if (defined $use_localtime) {
 	no warnings 'uninitialized';
 	local $^W; # shut off uninitialized warnings in Time::Local
-	$date = time2str("%Y-%m-%d %T %z", str2time ($date));
+	$date = reformat_svn_date("%Y-%m-%d %T %z", $date);
     }
     $author = loc('(no author)') if !defined($author) or !length($author);
     $output->print ($indent.$print_rev->($rev).":  $author | $date\n") unless $verbatim;

commit 5004c35fd16c0b26c359d07dfef3ccc63c4d98c0

Fix a win32 regression caused by r1763, which replaces Date::Format with POSIX strftime, which turned out to be not portable enough.
Revert to the old behaviour if we are on win32.  Also add Date::Format
back as a dependency for win32.


git-svn-id: file:///home/chmrr/work/bps/complete-history/svk/orig-svn/trunk@2360 4ccb8a5b-8ae0-0310-9b02-86531366991d

Makefile.PL
@@ -91,6 +91,9 @@ requires(
     'Pod::Simple'              => '0',       # in core since 5.9.3
     'File::Spec'               => '3.19',    # in core since 5.9.3
 );
+requires(
+    'Date::Format'             => '',
+) if ($^O eq 'MSWin32');
 build_requires(
     'Test::More'               => '0.42',
 );

lib/SVK/Util.pm
@@ -953,6 +953,11 @@ sub str2time {
 
 sub time2str {
     my ($format, $time) = @_;
+    if (IS_WIN32) {
+	require Date::Format;
+	goto \&Date::Format::time2str;
+    }
+
     require POSIX;
     return POSIX::strftime($format, localtime($time) );
 }