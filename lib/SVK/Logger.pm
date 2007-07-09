881a81180a42aee19f6d26140546609ab3b213c8
CL Sung <clsung@bestpractical.com>
2007-07-09 01:15:08 +0000

    - use SVKLOGCONFFILE env variable to specify the logger's configuration - testing
    git-svn-id: file:///home/chmrr/work/bps/complete-history/svk/orig-svn/branches/svk-logger@2425 4ccb8a5b-8ae0-0310-9b02-86531366991d

--- lib/SVK/Logger.pm
+++ lib/SVK/Logger.pm
@@ -62,7 +62,17 @@ if (eval {
     my $level = { map { $_ => uc $_ } qw( debug info warn error fatal ) }
         ->{ lc $ENV{SVKLOGLEVEL} } || 'INFO';
 
-    my $conf = qq{
+    my $conf_file = $ENV{SVKLOGCONFFILE};
+    my $conf;
+    if ( -e $conf_file ) {
+	my $FH;
+	open ($FH, $conf_file) or die $!;
+	local $/;
+	$conf = <$FH>;
+	close $FH;
+    }
+    #warn $conf unless $Log::Log4perl::Logger::INITIALIZED;
+    $conf ||= qq{
   log4perl.rootLogger=$level, Screen
   log4perl.appender.Screen = Log::Log4perl::Appender::Screen
   log4perl.appender.Screen.stderr = 0
