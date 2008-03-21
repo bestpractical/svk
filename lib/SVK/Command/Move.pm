90a8ea5c8c0f056b5fd12724d96d903828c12e5c
Chia-liang Kao <clkao@bestpractical.com>
2008-03-21 14:04:30 +0000

    Fix svk mv when we use tee editor to commit to remote.
    We are already doing the MapRev as part of the remote_editor,
    so we don't need to give delete_entry explicit rev anymore.
    
    
    git-svn-id: file:///home/chmrr/work/bps/complete-history/svk/orig-svn/trunk@2709 4ccb8a5b-8ae0-0310-9b02-86531366991d

--- lib/SVK/Command/Move.pm
+++ lib/SVK/Command/Move.pm
@@ -80,9 +80,8 @@ sub handle_direct_item {
     }
     $self->SUPER::handle_direct_item (@_, $call);
 
-    $editor->delete_entry (abs2rel ($src->path, $anchor => undef, '/'),
-			   $m ? scalar $m->find_remote_rev ($src->revision)
-			      : $src->revision, 0);
+    $editor->delete_entry(abs2rel ($src->path, $anchor => undef, '/'),
+                          $src->revision, 0);
     $self->adjust_anchor ($editor);
 }
 
