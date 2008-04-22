4e72676d6f72671c2b5fc86d07ed97909efc8115
Ruslan Zakirov <ruz@bestpractical.com>
2008-04-22 09:13:39 +0000

    * add initial implementation of path resolver
    git-svn-id: file:///home/chmrr/work/bps/complete-history/svk/orig-svn/branches/path-resolver@2745 4ccb8a5b-8ae0-0310-9b02-86531366991d

---
+++ lib/SVK/PathResolve.pm
@@ -0,0 +1,49 @@
+package SVK::PathResolve;
+use strict;
+use SVK::I18N;
+use SVK::Logger;
+use SVK::Util qw(get_prompt);
+
+sub new {
+    my ($class, %args) = @_;
+    return bless(\%args, $class);
+}
+
+sub add_file {
+    my ($self, $path) = @_;
+    
+    my ($prompt, $default);
+    my $default = 'a';
+    my $prompt = loc(
+        "Conflict found in %1:\na)add, s)kip, h)elp? [%2] ",
+        $path, $default
+    );
+
+    my $action = lc(get_prompt(
+        $prompt, qr/^[ash]?/i
+    ) || $default);
+    return $action if $action !~ /^h/;
+
+    die "help is not implemented yet";
+
+}
+
+sub add_directory {
+    my ($self, $path) = @_;
+    
+    my ($prompt, $default);
+    my $default = 'a';
+    my $prompt = loc(
+        "Conflict found in %1:\na)dd all, o)only this, s)kip, h)elp? [%2] ",
+        $path, $default
+    );
+
+    my $action = lc(get_prompt(
+        $prompt, qr/^[ash]?/i
+    ) || $default);
+    return $action if $action !~ /^h/;
+
+    die "help is not implemented yet";
+}
+
+1;
