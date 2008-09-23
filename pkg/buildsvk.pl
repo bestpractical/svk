2c662602a5a3c51c1719c4dc2bec4319d2d3f394
CL Sung <clsung@bestpractical.com>
2008-09-23 10:05:46 +0000

    - for Linux (ubuntu) libaprutil-1.so requiring libpq
    git-svn-id: file:///home/chmrr/work/bps/complete-history/svk/orig-svn/tags/2.2.0@3095 4ccb8a5b-8ae0-0310-9b02-86531366991d

--- pkg/buildsvk.pl
+++ pkg/buildsvk.pl
@@ -58,7 +58,7 @@ $build->prepare_svn_core();
 
 $build->build_module('libwin32') if $^O eq 'MSWin32';
 
-$build->build_module($_) for qw(Scalar-List-Utils Class-Autouse version Sub-Uplevel Test-Simple Test-Exception Data-Hierarchy PerlIO-via-dynamic PerlIO-via-symlink SVN-Simple PerlIO-eol Algorithm-Diff Algorithm-Annotate Pod-Escapes Pod-Simple IO-Digest TimeDate Getopt-Long Encode PathTools YAML-Syck Locale-Maketext-Simple App-CLI List-MoreUtils Path-Class Class-Data-Inheritable Class-Accessor UNIVERSAL-require File-Temp Log-Log4perl Time-Progress LWP-UserAgent);
+$build->build_module($_) for qw(Scalar-List-Utils Class-Autouse version Sub-Uplevel Test-Simple Test-Exception Data-Hierarchy PerlIO-via-dynamic PerlIO-via-symlink SVN-Simple PerlIO-eol Algorithm-Diff Algorithm-Annotate Pod-Escapes Pod-Simple IO-Digest TimeDate Getopt-Long Encode PathTools YAML-Syck Locale-Maketext-Simple App-CLI List-MoreUtils Path-Class Class-Data-Inheritable Class-Accessor UNIVERSAL-require File-Temp Log-Log4perl Time-Progress libwww-perl);
 $build->build_module($_) for qw(Locale-Maketext-Lexicon TermReadKey IO-Pager);
 $build->build_module($_) for qw(File-chdir SVN-Mirror);
 $build->build_module($_) for qw(FreezeThaw);
@@ -96,7 +96,9 @@ sub prepare_svn_core {
     my $output = `ldd \`which svn\``;
     for ($output =~ m/^.*$/mg) {
 	my ($lib, $file) = m/(\S.*?) => (\S.*?)\s/ or next;
-	if ($lib =~ m/libsvn_*/) {
+	if ($lib =~ m/libsvn_*/
+	    || $lib =~  m/libpq\./ # for aprutil in Linux (maybe just ubuntu?)
+	) {
 	warn "$lib $file";
 	    copy($file, $self->build_dir);
 	}
